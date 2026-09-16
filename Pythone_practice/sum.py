ef5bd9c4c29fedb36df064d3aac6dc5b5efcf710
souvik_biswas <souvikb395@gmail.com>
2026-09-16 18:02:58 +0000

    sum

---
+++ Pythone_practice/sum.py
@@ -0,0 +1,9 @@
+# Store user inputs and convert them to decimals (floats)
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Add the two numbers
+result = num1 + num2
+
+# Display the result using an f-string
+print(f"The sum of {num1} and {num2} is {result}")
