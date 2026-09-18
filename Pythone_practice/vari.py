c416db992d63531556e6d6cfc27ae18dcf5831a8
souvik_biswas <souvikb395@gmail.com>
2026-09-18 06:54:08 +0000

    varriable

---
+++ Pythone_practice/vari.py
@@ -0,0 +1,13 @@
+# learnig how to comment in pythone
+name1 = "souvik"
+age = 23
+
+print(name1, age )
+cgpa = 9.4
+isstudentc = True
+print(type(name1))
+print(type(age))
+print(type(cgpa))
+name = input("enter your name: ")
+print("hello", name)
+print("hello"+ name) #concatenation
