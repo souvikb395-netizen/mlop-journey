0cb1d157d9a17feffc18b51033972c9a30a590d9
souvik_biswas <souvikb395@gmail.com>
2026-09-20 18:21:23 +0000

    type

---
+++ Pythone_practice/sum1.py
@@ -0,0 +1,6 @@
+a = int(input("enter a"))
+b = int(input("enter b"))
+
+sum = a + b
+
+print (sum)
---
+++ Pythone_practice/type.py
@@ -0,0 +1,5 @@
+age = input("enter your age: ")
+new_age = int(age) + 1
+print(new_age)
+print(float(new_age))
+print(1 + 2.5)
