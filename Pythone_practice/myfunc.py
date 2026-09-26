73d75ed863fc09009179c888781e6391ad9c2f64
souvik_biswas <souvikb395@gmail.com>
2026-09-26 08:53:07 +0000

    name function

---
+++ Pythone_practice/myfunc.py
@@ -0,0 +1,13 @@
+def myfun(x ,y =50):
+    print("x: ", x)
+    print("y: " ,y)
+
+myfun(30)
+
+
+#function student
+def student(fname , lname):
+    print(fname , lname)
+
+student(fname='Souvik', lname='biswas')
+student(lname='biswas' , fname='soumika')
